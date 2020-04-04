d163e63a920398b99f0589e205b24deffcecec52
zimorodokan <zimorodokan@gmail.com>
2020-04-04 15:38:30 +0300

    'initial'

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ fp.update_preferences()
 
 # start Firefox browser
 # either using driver from 'executable_path' — Full path to geckodriver.exe
-# driver = Firefox(firefox_profile=fp, executable_path='geckodriver.exe')
+# driver = Firefox(firefox_profile=fp, executable_path="geckodriver.exe")
 # or if driver's location is available on the system PATH
 driver = Firefox(firefox_profile=fp)
 
@@ -35,14 +35,13 @@ with open("URLs.txt", encoding="utf8") as file:
         # start and end time is used for useful logging of adding process
         # every line of file has to be a URL for example https://example.org/example.html
         line = line.rstrip()
-        print(time.strftime("%H:%M:%S", time.localtime()), line, "start")
+        print(time.strftime("%H:%M:%S", time.localtime()), line, "start page loading")
         driver.get("https://web.archive.org/save/" + line)
-
         # you may check what you want in Web Page Title for errors, for example the same for every page part of a Title
         assert stringForChecking in driver.title
-
-        print(time.strftime("%H:%M:%S", time.localtime()), line, "end")
-
+        print(time.strftime("%H:%M:%S", time.localtime()), line, "end page loading", driver.current_url)
         # pause after adding of one url to avoid your IP blocking
         time.sleep(60)
+        assert stringForChecking in driver.title
+        print(time.strftime("%H:%M:%S", time.localtime()), "page added", line, driver.current_url)
 driver.quit()
