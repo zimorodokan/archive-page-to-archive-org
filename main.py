2e3d86bdba62e420b7dc6240b975be19d2f488d0
zimorodokan <zimorodokan@gmail.com>
2020-08-06 18:39:25 +0300

    reformat

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ fp.set_preference("privacy.trackingprotection.enabled", True)
 fp.set_preference("privacy.trackingprotection.fingerprinting.enabled", True)
 fp.set_preference("privacy.trackingprotection.cryptomining.enabled", True)
 fp.set_preference("privacy.trackingprotection.socialtracking.enabled", True)
-fp.set_preference("network.cookie.cookieBehavior", 3)  # http://kb.mozillazine.org/Network.cookie.cookieBehavior
+# http://kb.mozillazine.org/Network.cookie.cookieBehavior
+fp.set_preference("network.cookie.cookieBehavior", 3)
 fp.update_preferences()
 
 # start Firefox browser
@@ -27,7 +28,8 @@ driver = Firefox(firefox_profile=fp)
 # waiting before adding first url to complete browser start
 time.sleep(30)
 
-stringForChecking = "Example Domain"  # replace 'Example Domain' with what you want
+# replace 'Example Domain' with what you want
+stringForChecking = "Example Domain"
 
 # create a utf-8 encoded file URLs.txt with urls, which you want to add to archive.org, one url per line
 with open("URLs.txt", encoding="utf8") as file:
@@ -35,13 +37,16 @@ with open("URLs.txt", encoding="utf8") as file:
         # start and end time is used for useful logging of adding process
         # every line of file has to be a URL for example https://example.org/example.html
         line = line.rstrip()
-        print(time.strftime("%H:%M:%S", time.localtime()), line, "start page loading")
+        print(time.strftime("%H:%M:%S", time.localtime()),
+              line, "start page loading")
         driver.get("https://web.archive.org/save/" + line)
         # you may check what you want in Web Page Title for errors, for example the same for every page part of a Title
         assert stringForChecking in driver.title
-        print(time.strftime("%H:%M:%S", time.localtime()), line, "end page loading", driver.current_url)
+        print(time.strftime("%H:%M:%S", time.localtime()),
+              line, "end page loading", driver.current_url)
         # pause after adding of one url to avoid your IP blocking
         time.sleep(60)
         assert stringForChecking in driver.title
-        print(time.strftime("%H:%M:%S", time.localtime()), "page added", line, driver.current_url)
+        print(time.strftime("%H:%M:%S", time.localtime()),
+              "page added", line, driver.current_url)
 driver.quit()
